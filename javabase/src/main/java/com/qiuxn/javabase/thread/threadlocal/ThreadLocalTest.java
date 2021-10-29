ad8a3b873467130a7df98c5b666155891153b404
qiuxiangneng <1050533450@qq.com>
2021-10-30 00:44:27 +0800

    【代码提交】springboot demo

---
+++ javabase/src/main/java/com/qiuxn/javabase/thread/threadlocal/ThreadLocalTest.java
@@ -0,0 +1,34 @@
+package com.qiuxn.javabase.thread.threadlocal;
+
+public class ThreadLocalTest {
+    static ThreadLocal<String> threadLocal = new ThreadLocal<String>();
+
+    private static void print(String str){
+        //打印当前线程 本次内存中的threadLocal变量的值
+        System.out.println(str + ":" + threadLocal.get());
+        //清除当前线程 本次内存中的threadLocal变量的值
+        //threadLocal.remove();
+    }
+
+    public static void main(String[] args) {
+        Thread threadOne = new Thread(new Runnable() {
+            public void run() {
+                threadLocal.set("threadOne threadLocal value");
+                print("threadOne");
+                System.out.println("threadOne after remove:"+threadLocal.get());
+            }
+        });
+
+
+        Thread threadTwo = new Thread(new Runnable() {
+            public void run() {
+                threadLocal.set("threadTwo threadLocal value");
+                print("threadTwo");
+                System.out.println("threadTwo after remove:"+threadLocal.get());
+            }
+        });
+        threadOne.start();
+        threadTwo.start();
+
+    }
+}
---
+++ javabase/src/test/java/com/qiuxn/javabase/JavabaseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.qiuxn.javabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
